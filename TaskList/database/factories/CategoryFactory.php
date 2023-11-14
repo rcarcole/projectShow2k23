b736ac9c8e3b90cf76fb7e68a348699a3a081ebf
robert <rcarcole@hotmail.com>
2023-11-14 17:38:48 +0100

    add migrationws, factorys, seeders

---
+++ TaskList/database/factories/CategoryFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->words(1, true),
+            'owner_id' => fake()->numberBetween(1, 10),
+            'created_at' => fake()->dateTime(),
+        ];
+    }
+}
